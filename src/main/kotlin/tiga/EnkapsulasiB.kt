75992ecdda4c0decbf906a325090f0022f764972
Noval <mnovalhidayat298@gmail.com>
2022-03-24 14:10:15 +0700

    Contoh Enkapsulasi

--- src/main/kotlin/tiga/EnkapsulasiB.kt
+++ src/main/kotlin/tiga/EnkapsulasiB.kt
@@ -1,5 +1,11 @@
 package tiga
-//eknkapsulasi b
+//Terdapat dua keyword asing yaitu value dan field. Value adalah nama parameter setter. Field atau
+//disebut juga backing fields membantu kita untuk merujuk property didalam method getter dan
+//setter. Field ini sangat dibutuhkan, jika kita menggunakan property secara langsung didalam
+//method getter dan setter maka kita akan memanggil program secara rekursif dan menyebabkan
+//error StackOverflowError.
+//Kita juga dapan merubah method setter dan getter default, sesuai dengan keinginan kita seperti
+//berikut:
 class EmployeeA(_id: Int, _name: String, _age: Int) {
     val id: Int = _id
         get() = field
