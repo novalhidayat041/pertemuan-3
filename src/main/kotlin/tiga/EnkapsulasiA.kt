2f35f80b7749d84079abbcfa90f80a3b171914ec
Noval <mnovalhidayat298@gmail.com>
2022-03-24 14:09:13 +0700

    Contoh Enkapsulasi

--- src/main/kotlin/tiga/EnkapsulasiA.kt
+++ src/main/kotlin/tiga/EnkapsulasiA.kt
@@ -2,7 +2,7 @@ package tiga
 //Inisialisasi sintaks getter dan setter di Kotlin bersifat opsional, karena secara sefault Kotlin telah
 //men-generate method getter dan setter untuk property yang mutable (var) dan hanya getter
 //untuk property yang read-only (val). Secara default kode yang ter-generate secara otomatis
-//tersebut adalah seperti pada contoh berikut :
+//tersebut adalah .seperti pada contoh berikut :
 class Employe(_id: Int, _name: String, _age: Int) {
     val id: Int = _id
         get() = field
