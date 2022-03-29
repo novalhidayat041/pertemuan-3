67e034c7bd743f9cfd0dd8a7ea6e93ed327968a5
Noval <mnovalhidayat298@gmail.com>
2022-03-29 20:49:54 +0700

    Contoh-contoh inheritance

---
+++ src/main/kotlin/EmpatInheritance/InheritanceE.kt
@@ -0,0 +1,22 @@
+package EmpatInheritance
+
+//Overriding Property’s Getter/Setter method
+//Kita dapat meng-override property kelas menggunakan inisialisasi atau menggunakan custom
+//setter dan getter function.
+
+open class NewPerson {
+    open var age: Int = 1
+}
+
+class CheckedPerson: NewPerson() {
+    override var age: Int = 1
+        set(value) {
+            field = if(value > 0) value else throw IllegalArgumentException("Age can not be negative")
+        }
+}
+fun main() {
+    val person = NewPerson()
+    person.age = -5 // Works
+    val checkedPerson = CheckedPerson()
+    checkedPerson.age = -5 // Throws IllegalArgumentException : Age can not be negative
+}
