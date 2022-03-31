99c59458e78edd4fa70e3a92b57729e0caa07ee6
Noval <mnovalhidayat298@gmail.com>
2022-03-31 11:23:11 +0700

    Contoh-contoh Access Interface

---
+++ src/main/kotlin/TujuhInterface/InterfaceA.kt
@@ -0,0 +1,23 @@
+package TujuhInterface
+
+interface MyInterface{
+    var str: String
+    fun demo()
+    fun func(){
+        println("Hello")
+    }
+}
+
+class MyClass: MyInterface{
+    override var str: String = "Rekayasa Perangkat Lunka"
+    override fun demo() {
+        println("Demo Function")
+    }
+}
+
+fun main() {
+    val obj = MyClass()
+    obj.demo()
+    obj.func()
+    println(obj.str)
+}
