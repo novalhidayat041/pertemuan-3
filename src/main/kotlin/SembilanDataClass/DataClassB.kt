d2464da774b3e07d795b7b0eeea16e017a3482a2
Noval <mnovalhidayat298@gmail.com>
2022-03-31 11:46:57 +0700

    Contoh-contoh Data Class

---
+++ src/main/kotlin/SembilanDataClass/DataClassB.kt
@@ -0,0 +1,23 @@
+package SembilanDataClass
+
+//Data class hashCode() and equals() methods
+//Jika dua buah objek memiliki referensi objek yang sama, maka kedua objek tersebut juga
+//memiliki nilai hashCode yang sama. Kita bisa membandingkan nilai kode hash 2 objek apakah
+//sama atau tidak dengan menggunakan fungsi hashCode()
+
+fun main() {
+    val boni = Student("Boni", 21)
+    val newBoni = Student("Boni", 21)
+    val meri = Student("Meri", 20)
+    if (boni.equals(newBoni))
+        println("boni is equal to newBoni.")
+    else
+        println("boni is not equal to newBoni.")
+    if (boni.equals(meri))
+        println("boni is equal to meri.")
+    else
+        println("boni is not equal to meri.")
+    println("Hashcode of boni: ${boni.hashCode()}")
+    println("Hashcode of newBoni: ${newBoni.hashCode()}")
+    println("Hashcode of meri: ${meri.hashCode()}")
+}
