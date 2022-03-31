22de9fe1acc1759f653f8cfaf31db494e0862105
Noval <mnovalhidayat298@gmail.com>
2022-03-31 11:09:17 +0700

    Contoh-contoh Access Modifier

--- src/main/kotlin/Enam/AccesModifierA.kt
+++ src/main/kotlin/Enam/AccesModifierA.kt
@@ -1,6 +1,4 @@
 package Enam
-//Kelas Abstrak adalah kelas yang tidak bisa di Instansiasi menjadi sebuah objek, akan tetapi ia
-//bisa diwariskan ke kelas yang lainnya.
 
 open class Mahasiswa(var nama: String, var nrp: Int){
     open fun info() {
